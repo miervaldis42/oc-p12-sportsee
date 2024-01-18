c4d041e11c2611d71b1cd119f3e395b8a52dc6f3
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-01-18 16:00:49 +0400

    feat (client) : API Handler Update
    Add a new case to apply some modifications to the data before sending the response back to the front-end

--- client/src/services/apiHandler.js
+++ client/src/services/apiHandler.js
@@ -22,6 +22,50 @@ async function apiHandler(userId, apiRoute) {
           case "average-sessions":
             const userActivityData = data.sessions;
             return [true, userActivityData];
+          case "performance":
+            const userPerformanceData = {
+              performances: [],
+              kind: {},
+            };
+
+            // Transform performances array
+            data.data.forEach((item) => {
+              userPerformanceData.performances.push({
+                value: item.value,
+                kind: item.kind,
+              });
+            });
+
+            // Transform kind object
+            Object.keys(data.kind).forEach((key) => {
+              userPerformanceData.kind[key] = {
+                name: translateKindInto(data.kind[key], "fr"),
+                order: 7 - parseInt(key), // As the order is in reverse (6, 5, 4, 3, 2, 1)
+              };
+            });
+
+            function translateKindInto(kind, language) {
+              if (language === "fr") {
+                switch (kind) {
+                  case "cardio":
+                    return "Cardio";
+                  case "energy":
+                    return "Energie";
+                  case "endurance":
+                    return "Endurance";
+                  case "strength":
+                    return "Force";
+                  case "speed":
+                    return "Vitesse";
+                  case "intensity":
+                    return "Intensité";
+                  default:
+                    return;
+                }
+              }
+            }
+
+            return [true, userPerformanceData];
           default:
             return;
         }
