76999ac3cfe0b1fbd96828bfce6d39cf61f55dcb
miervaldis42 <31883227+miervaldis42@users.noreply.github.com>
2024-01-20 16:17:37 +0400

    feat (client) : Mock Data & API Handler
    - Mock data in a file to be used instead of the server in dev mode
    - Update the content of API handler to handle the mock data when `REACT_APP_DATABASE` is present
    - Format the data based on the data & the client router route

--- client/src/services/apiHandler.js
+++ client/src/services/apiHandler.js
@@ -4,42 +4,94 @@ import Activities from "../schemas/Activities";
 import AverageSessions from "../schemas/AverageSessions";
 import Performances from "../schemas/Performances";
 
+// Mock Data
+import {
+  USER_MAIN_DATA,
+  USER_ACTIVITY,
+  USER_AVERAGE_SESSIONS,
+  USER_PERFORMANCE,
+} from "../data/fake-data";
+
 // Services - API
 async function apiHandler(userId, apiRoute) {
   const url = "http://localhost:3000/user";
   const apiRoutes = ["", "activity", "average-sessions", "performance"];
 
+  const givenUserId = Number(userId);
+
   if (apiRoutes.includes(apiRoute)) {
     try {
-      const response = await fetch(`${url}/${userId}/${apiRoute}`);
+      let response = {};
 
-      if (response.ok) {
-        const res = await response.json();
-        const data = await res.data;
+      // Data are retrieved from the server or mock data
+      if (process.env.REACT_APP_DATABASE === undefined) {
+        response = await fetch(`${url}/${userId}/${apiRoute}`);
 
-        switch (apiRoute) {
-          case "":
-            const formattedUserData = new User(data);
-            return [true, formattedUserData];
-          case "activity":
-            const userActivityData = new Activities(data);
-            return [true, userActivityData];
-          case "average-sessions":
-            const userSessionsData = new AverageSessions(data);
-            return [true, userSessionsData];
-          case "performance":
-            const userPerformanceData = new Performances(data);
-            return [true, userPerformanceData];
-          default:
-            return;
+        if (response.ok) {
+          const res = await response.json();
+          const data = await res.data;
+
+          switch (apiRoute) {
+            case apiRoutes[0]:
+              const formattedUserData = new User(data);
+              return [true, formattedUserData];
+            case apiRoutes[1]:
+              const userActivityData = new Activities(data);
+              return [true, userActivityData];
+            case apiRoutes[2]:
+              const userSessionsData = new AverageSessions(data);
+              return [true, userSessionsData];
+            case apiRoutes[3]:
+              const userPerformanceData = new Performances(data);
+              return [true, userPerformanceData];
+            default:
+              return;
+          }
+        } else {
+          let customError = {
+            status: response.status,
+            description: response.statusText,
+          };
+
+          return [false, customError];
         }
       } else {
-        let customError = {
-          status: response.status,
-          description: response.statusText,
-        };
+        switch (apiRoute) {
+          case apiRoutes[0]:
+            const userData = USER_MAIN_DATA.find(
+              (user) => user.id === givenUserId
+            );
+            const formattedUserData = new User(userData);
 
-        return [false, customError];
+            return [true, formattedUserData];
+          case apiRoutes[1]:
+            const userActivities = USER_ACTIVITY.find(
+              (user) => user.userId === givenUserId
+            );
+            const formattedUserActivities = new Activities(userActivities);
+
+            return [true, formattedUserActivities];
+          case apiRoutes[2]:
+            const userAverageSessions = USER_AVERAGE_SESSIONS.find(
+              (user) => user.userId === givenUserId
+            );
+            const formattedUserAverageSessions = new AverageSessions(
+              userAverageSessions
+            );
+
+            return [true, formattedUserAverageSessions];
+          case apiRoutes[3]:
+            const userPerformances = USER_PERFORMANCE.find(
+              (user) => user.userId === givenUserId
+            );
+            const formattedUserPerformances = new Performances(
+              userPerformances
+            );
+
+            return [true, formattedUserPerformances];
+          default:
+            return [false, "erreur"];
+        }
       }
     } catch (error) {
       console.error(error);
